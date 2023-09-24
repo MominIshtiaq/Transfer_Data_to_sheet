d76d783868c3e947d1cdfadc9df82b490140e69b
MominIshtiaq <momin989ishtiaq@gmail.com>
2023-09-24 20:57:53 +0500

    javascript created now debugging begins

---
+++ index.js
@@ -0,0 +1,19 @@
+let url =
+  "https://script.google.com/macros/s/AKfycbzoh82evLQSpOpPJ004_eU-2XVQNgY1aMKo4yvwuyAuq8mdWThEV61aQEjSwqExRRYAkw/exec";
+
+const form = document.querySelector(".form");
+form.addEventListener("submit", (e) => {
+  console.log(e.target.btn);
+  //   let formData = new FormData(form);
+  //   fetch(url, {
+  //     method: "POST",
+  //     body: formData,
+  //   })
+  //     .then((res) => res.text())
+  //     .then((finalResponse) => {
+  //       let span = document.createElement("span");
+  //       span = document.createTextNode(finalResponse);
+  //       document.querySelector(".res_result").append(span);
+  //     });
+  //   e.preventDefault();
+});
